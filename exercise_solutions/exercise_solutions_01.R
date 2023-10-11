f27452d4be2099b19fc4937fba245ed21995d180
nrennie <nrennie35@gmail.com>
2023-10-11 23:03:15 +0100

    add exercise 1

--- exercise_solutions/exercise_solutions_01.R
+++ exercise_solutions/exercise_solutions_01.R
@@ -1,16 +1,28 @@
 
 # Load R packages ---------------------------------------------------------
 
+library(tidyverse)
+library(tidymodels)
+tidymodels_prefer()
 
 
 # Load the `exercise.csv` data --------------------------------------------
 
+exercise <- read_csv("data/exercise.csv")
 
 
 # Inspect variables -------------------------------------------------------
 
+View(exercise)
+barplot(table(exercise$SAE.death))
+barplot(table(exercise$Geschlecht))
+hist(exercise$Bas_BMI)
 
 
 # Split into training and testing -----------------------------------------
 # Choose your own proportion for the split!
 
+set.seed(20231018)
+ex_split <- initial_split(exercise, prop = 0.7)
+ex_train <- training(ex_split)
+ex_test <- testing(ex_split)
