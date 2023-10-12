8a1321a49c6769266d91e972e818a1dc3a9172ca
nrennie <nrennie35@gmail.com>
2023-10-12 13:20:51 +0100

    add exercise 2

---
+++ exercises/exercise_02.R
@@ -0,0 +1,25 @@
+
+# Build a recipe ----------------------------------------------------------
+
+
+# Specify the model -------------------------------------------------------
+
+
+# Tune the model ----------------------------------------------------------
+
+# Create cross validation folds
+
+# Fit lots of values
+
+# Choose the best value
+
+
+# Fit the final model -----------------------------------------------------
+
+
+# Model evaluation --------------------------------------------------------
+
+
+# which variables were most important?
+
+
