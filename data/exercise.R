0bc071a31dc04fcbb5bacdeec5a49784b9991ed6
nrennie <nrennie35@gmail.com>
2023-10-12 13:00:05 +0100

    tidy up exercise data

--- data/exercise.R
+++ data/exercise.R
@@ -10,7 +10,15 @@ load(url("https://zenodo.org/record/3899830/files/safety-set.RData"))
 exercise <- safety.set |>
   as_tibble() |>
   select(where(~ !any(is.na(.x)))) |> 
-  relocate(SAE.death)
+  rename(Age = Bas_Alter,
+         Gender = Geschlecht,
+         SAE = subgroup,
+         Centre = Zentrum) |> 
+  select(-c(SAE.hosp.time.Pat, patient.months.V3,
+            SAE.death, Bas_SA_Lok_Seite, Bas_SA_Lok_hinter, obs.time)) |> 
+  rename_with(~ gsub("Bas_", "", .x, fixed = TRUE)) |> 
+  select(c(where(is.numeric), where(is.factor))) |> 
+  relocate(SAE)
 
 
 # Write to CSV ------------------------------------------------------------
