493b6ac4d1c504834d12af03edd27222424680ce
nrennie <nrennie35@gmail.com>
2023-10-12 12:34:27 +0100

    add examples 2

---
+++ examples/example_02.R
@@ -0,0 +1,60 @@
+
+# Build a recipe ----------------------------------------------------------
+
+hf_recipe <- recipe(death ~ ., data = hf_train) |> 
+  step_dummy(sex) |> 
+  step_normalize(all_numeric())
+
+wf <- workflow() |> 
+  add_recipe(hf_recipe)
+
+
+# Specify the model -------------------------------------------------------
+
+tune_spec <- logistic_reg(penalty = tune(), mixture = 1) |>
+  set_engine("glmnet")
+
+
+# Tune the model ----------------------------------------------------------
+
+# Create cross validation folds
+set.seed(20231018)
+hf_folds <- vfold_cv(hf_train, v = 10)
+
+# Fit lots of values
+lasso_grid <- tune_grid(
+  add_model(wf, tune_spec),
+  resamples = hf_folds,
+  grid = grid_regular(penalty(), levels = 50)
+)
+
+# Choose the best value
+highest_roc_auc <- lasso_grid |>
+  select_best("roc_auc")
+
+
+# Fit the final model -----------------------------------------------------
+
+final_lasso <- finalize_workflow(
+  add_model(wf, tune_spec),
+  highest_roc_auc
+)
+
+
+# Model evaluation --------------------------------------------------------
+
+last_fit(final_lasso, hf_split) |>
+  collect_metrics()
+
+# which variables were most important?
+final_lasso |>
+  fit(hf_train) |>
+  extract_fit_parsnip() |>
+  vip::vi(lambda = highest_roc_auc$penalty) |>
+  mutate(
+    Importance = abs(Importance),
+    Variable = fct_reorder(Variable, Importance)
+  ) |>
+  ggplot(mapping = aes(x = Importance, y = Variable, fill = Sign)) +
+  geom_col()
+
---
+++ examples_blank/example_02.R
@@ -0,0 +1,28 @@
+
+# Build a recipe ----------------------------------------------------------
+
+
+# Specify the model -------------------------------------------------------
+
+
+# Tune the model ----------------------------------------------------------
+
+
+# Create cross validation folds
+
+
+# Fit lots of values
+
+
+# Choose the best value
+
+
+# Fit the final model -----------------------------------------------------
+
+
+# Model evaluation --------------------------------------------------------
+
+
+# which variables were most important?
+
+
