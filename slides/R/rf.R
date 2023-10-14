ff0f1a1fbc2ea87e332eb5b522a6addcf50aeb29
nrennie <nrennie35@gmail.com>
2023-10-14 22:17:33 +0100

    minor fixes

--- slides/R/rf.R
+++ slides/R/rf.R
@@ -15,11 +15,11 @@ node_data <- tibble::tibble(name = unique(c(plot_data$from, plot_data$to))) %>%
   dplyr::mutate(label = gsub("\\d+$", "", name),
                 colour = c(rep("white", 3), "#bec0c2", "#b20e10", "#bec0c2", "#b20e10"))
 ggflowchart(plot_data, node_data, fill = colour,
-            alpha = 0.5, x_nudge = 0.4,
-            text_size = 5) +
+            alpha = 0.5, x_nudge = 0.5,
+            text_size = 7) +
   labs(title = "Should I walk to work?") +
   scale_fill_identity() +
-  theme_void(base_size = 22)
+  theme_void(base_size = 24)
 ggsave("slides/images/rf_01.png", width = 4, height = 4, units = "in")
 
 
